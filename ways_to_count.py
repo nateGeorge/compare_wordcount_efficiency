4012efb8f88d0c24ebd8555ba35230e0bf13d7be
nateGeorge <wordsforthewise@gmail.com>
2019-10-23 17:00:41 -0600

    fix typo, add a few more things

--- ways_to_count.py
+++ ways_to_count.py
@@ -13,11 +13,15 @@ c.keys()
 fd = nltk.FreqDist(t)
 # unique words
 fd.keys()
+# you can plot the distribution easily
+fd.plot()
+# get words that occur at least a certain number of times
+more_than_once = [(f, c) for f, c in fd.items() if c > 1]
 
 # this method is more useful for multiple documents
 vec = CV()
 res = vec.fit_transform([text])
 # same result as counter/FreqDist here
-voc.vocabulary_
+vec.vocabulary_
 # get unique words
 vec.vocabulary_.keys()
